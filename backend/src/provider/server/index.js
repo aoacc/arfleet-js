6c49c3126047887743d752beeafcb23dc118133d
Niko Storni <niko@odysee.com>
2024-08-30 14:10:46 +0200

    add cors

--- backend/src/provider/server/index.js
+++ backend/src/provider/server/index.js
@@ -9,6 +9,7 @@ const nodepath = require('path');
 const fs = require('fs');
 const mime = require('mime-types');
 const arbundles = require('arbundles');
+const cors = require('cors')
 
 let state = {};
 
@@ -118,6 +119,7 @@ const startPublicServer = async() => {
             const express = require('express');
             const bodyParser = require('body-parser');
             const app = express();
+            app.use(cors())
 
             // app.use(express.json());
             app.use(express.json({ limit: '10mb' }));
